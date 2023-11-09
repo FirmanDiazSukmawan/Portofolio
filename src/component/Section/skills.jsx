d6fc09980de3e78af44937f213b2e682b4d90854
xTats <firman670676@gmail.com>
2023-11-10 02:56:41 +0700

    repair width skills

--- src/component/Section/skills.jsx
+++ src/component/Section/skills.jsx
@@ -8,7 +8,7 @@ function Skills() {
       <div className="text-white font-sora text-5xl ">MySkills</div>
       <div className="flex flex-row flex-wrap justify-center">
         {Object?.keys(skillImage)?.map((index)=>(
-        <div className="lg:w-[150px] md:w-[125px] w-[75px] lg:h-[125px] md:h-[125px] h-[75px]  rounded-[20px] flex flex-row space-y-1 justify-between mx-4 my-2" key={index}>
+        <div className="lg:w-[175px] md:w-[130px] w-[75px] lg:h-[125px] md:h-[125px] h-[75px]  rounded-[20px] flex flex-row space-y-1 justify-between mx-4 my-2" key={index}>
               <img src={skillImage[index].image} alt="html" className="w-full h-full object-fill brightness-100"/>
         </div>
         ))}
